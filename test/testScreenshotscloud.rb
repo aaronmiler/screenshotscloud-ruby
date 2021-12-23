78aa479a808cc1d3a4ffc91bc5168d84a29bed18
Aaron Miler <aaron@aaronmiler.com>
2021-12-23 11:53:12 -0800

    Fix the URI decode deprication

--- test/testScreenshotscloud.rb
+++ test/testScreenshotscloud.rb
@@ -2,13 +2,15 @@ require './lib/screenshotscloud.rb'
 require 'test/unit'
 
 class TestScreenshotscloud < Test::Unit::TestCase
-	def test_match_expected_output
-		screenshotscloud = ScreenshotsCloud.new("my-key-generated-at-screenshots-dot-cloud", "mysecretstringgeneratedatscreenshotsdotcloud");
+  def test_match_expected_output
+    url = api_client.screenshot_url({
+      "url" => "bbc.com/news"
+    })
 
-		url = screenshotscloud.screenshotUrl({
-			"url" => "bbc.com/news"
-		})
+    assert(url == "https://api.screenshots.cloud/v1/screenshot/my-key-generated-at-screenshots-dot-cloud/3fc4e5cd418052343e3dc61b2eaf17bec53adaeb?url=bbc.com%2Fnews", "Expected output does not match")
+  end
 
-		assert(url == "https://api.screenshots.cloud/v1/screenshot/my-key-generated-at-screenshots-dot-cloud/3fc4e5cd418052343e3dc61b2eaf17bec53adaeb?url=bbc.com%2Fnews", "Expected output does not match")
-	end
+  def api_client
+    ScreenshotsCloud.new("my-key-generated-at-screenshots-dot-cloud", "mysecretstringgeneratedatscreenshotsdotcloud");
+  end
 end
